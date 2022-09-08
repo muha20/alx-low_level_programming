8b541c93be5d9f776ddfdd2d3a1ca1b3e41afd47
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-08 15:09:12 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+* Desciption: using the main function
+* this program prints "Programing is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	printf ("with proper grammer, but the outcome is a place of art,\n");
+	return (0);
+}
