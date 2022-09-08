4601ae801774781e5bf11d3a1971e0f05d6f5c35
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-08 15:54:19 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                              
+#include <stdio.h>
 
-/**                                                                             
- * main - print the string in the put function         
- * 
- * Description: using the main function  
- * this program prints "Programing is like building a multilingual puzzle"
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programing is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
