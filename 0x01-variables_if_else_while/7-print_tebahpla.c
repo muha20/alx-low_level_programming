4c2d1b3f26d308a2e7f8afbac6a6a03f31b8dfe8
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-12 13:33:37 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or nagative
+ * Description: using the main function
+ * this programe prints "Programming is positive, zero, or nagative
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
