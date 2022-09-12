24dd74a9100937f35b2d446b008912a461bbe185
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-12 04:38:39 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@
 int main(void)
 
 {
-		int n;
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-					return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+return (0);
 
 }
 if (n > 0)
