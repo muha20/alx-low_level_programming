4594978288c539ed6a7a865c999ff66dd723eaec
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-12 03:11:41 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,20 @@
 int main(void)
 {
 	char c = 'a';
-	while (c < = 'z')
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
+
 	c ='A';
-	while (c < = 'Z')
+
+	while (c <= 'Z')
 	{
 		putchar(c);
 		c++;
 	}
+
 	putchar('\n');
 	return (0);
 }
