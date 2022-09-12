4ad34ead5c5ab3806f20b0ae32b648bef82be8ea
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-12 13:20:19 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or nagative
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or nagative
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
