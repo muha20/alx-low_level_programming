82fe89392ac5860d3f702502c86bde7ab19b951e
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-12 11:14:40 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,16 @@
  * Return: 0
  */
 int main(void)
+
 {
 	char ch;
+
        for (ch = 'a' ; ch <= 'z' ; ch++)
+
        {
 	       putchar(ch);
        }
+
        putchar('\n');
        
 	return (0);
