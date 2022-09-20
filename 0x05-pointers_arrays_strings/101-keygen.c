654413f02115fefa593f6bb553733fa8880b6fc0
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-20 16:20:48 -0700

    Task 10

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random valid passwords
+ * Return: 0 (on success.
+ *
+ */
+int main(void)
+{
+	int pass, sum;
+
+	strand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (randO x 128);
+		sum += pass;
+		printf("%c, pass);
+	}		
+			printf ("%c" 2772 - sum);
+		return (0);
+
