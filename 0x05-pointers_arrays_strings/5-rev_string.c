e377db03da918486edfd2c1bf6e58b0ed84048d7
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-20 14:33:50 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,15 @@
  * Return: void.
  */
 void rev_string(char *s)
-{ int i, j, k, temp;
+{
+       	int i, j, k, temp;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	k 0;
+	k = 0;
 	j = i - 1;
 
 	while (k < j)
