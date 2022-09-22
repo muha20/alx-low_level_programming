e410d5b7f65b8832455a2ac3f3ef9c99b25319f7
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-22 10:01:20 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,15 +5,15 @@
  * @s1: first string
  * @s2: second string
  * Return:
- * 	returns zero if s1 ==s2
- * 	returns nagative number if s1 < s2
- * 	returns positive number if s1 > s2
+ * returns zero if s1 ==s2
+ * returns nagative number if s1 < s2
+ * returns positive number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, diff = 0;
-	
+
 	while (1)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
