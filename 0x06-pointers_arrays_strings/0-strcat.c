ba61dce2f77c2f61e1abfeb184727c82054f2be1
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-22 06:46:01 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminat
  * null byte, to the end of the string pointed to by @dest.
