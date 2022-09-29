6104db89bd161f61824536544094bfda1fff92d7
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-29 05:24:22 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,7 +10,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;
-	
+
 	while (haystack[i])
 	{
 		while (needle[j] && (haystack[i] == needle[0]))
@@ -19,13 +19,11 @@ char *_strstr(char *haystack, char *needle)
 				j++;
 			else
 				break;
-	}
-	if (needle[j])
-	{
+		}
+		if (needle[j])
 		{
 			i++;
 			j = 0;
-
 		}
 		else
 			return (haystack + 1);
