ec0dcf0bc563dd60467d770ae8b4467ab98b3e25
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-09-29 11:06:00 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,6 +19,8 @@ int is_prime_number(int n)
  * is_prime - returns the 1 if n is prime
  * @n: number to be checked
  * @start: number to start checking from
+ *
+ * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime(int n, int start)
 {
