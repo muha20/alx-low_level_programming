02432d8186957d9e275220b9172ce02ce444405f
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-10-03 13:43:59 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,11 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+	return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
@@ -31,4 +35,4 @@ int **alloc_grid(int width, int height)
 		for (i = 0; j < width; j++)
 			grid[i][j] = 0;
 		return (grid);
-	}
+}
