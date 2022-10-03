297d971c4eae5a4cca33d2472e25fe7e418d756c
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-10-03 12:41:47 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,9 +28,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, size;
 
 	if (s1 == NULL)
-		s1 - "";
+		s1 = "";
 	if (s2 == NULL)
-		s2 - "";
+		s2 = "";
 	size = (_strlen[s1] + _strlen [s2] + 1);
 
 	dst = (char *) malloc(size * sizeof(char));
@@ -43,7 +43,7 @@ char *str_concat(char *s1, char *s2)
 		*(dst + i) = *(s1 + i);
 	for(j = 0; *(s2 + j) != '\0'; j++)
 	{
-		*(dst + i) = *(s2 * j);
+		*(dst + i) = *(s2 + j);
 		i++;
 	}
 	return (dst);
