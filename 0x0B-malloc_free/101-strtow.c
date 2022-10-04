baadae53bca24ccbb268bcc8cbabaafbbdde996d
Aliyu Muhammed <muhammed20aliyu@gmail.com>
2022-10-04 08:37:55 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -61,7 +61,7 @@ char **strtow(char *str)
 	if (words == 0)
 		return (NULL);
 
-	string = malloc(sizeof(char *) * (words + 1));
+	strings = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
 		return (NULL);
 	for (w = 0; w < words; w++)
